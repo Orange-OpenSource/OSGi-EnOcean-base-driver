3edfd0bab261c3768acf464e645a47a1fc229c21
antonin.chazalet@orange.com <antonin.chazalet@orange.com>
2014-11-19 14:40:02 +0100

    Remove a non-needed dependancy in bnd.bnd file. Delete a no longer needed TODO tasks.

--- com.orange.impl.service.enocean/src/com/orange/impl/service/enocean/EnOceanBundleActivator.java
+++ com.orange.impl.service.enocean/src/com/orange/impl/service/enocean/EnOceanBundleActivator.java
@@ -35,10 +35,6 @@ public class EnOceanBundleActivator implements BundleActivator {
 	private EnOceanBaseDriver basedriver;
 
 	public void start(BundleContext bc) {
-
-		// TODO AAA: Somewhere, the base-driver should register an "fake"
-		// EnOcean switch device that representes the base-driver itself...
-
 		basedriver = new EnOceanBaseDriver(bc);
 		basedriver.start();
 	}
